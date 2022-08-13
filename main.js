3a9460d39356ab1bd45f226c6f6cdc0118b32c44
Taa <natanaelchacoma701@gmail.com>
2022-08-12 21:17:12 -0300

    Con jquery

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
+$(function(){
 
+$("p").css({"background-color":"black"});
+$("p").css({"color":"white"});
+
+$("#btn-color").click(function(){
+
+    $("p").css({"background-color":"white"});
+    $("p").css({"color":"black"});
+})
+
+$("#btn-hideShow").click(function(){
+
+    $("p").slideUp();
+    $("#btn-hideShow").text("Mostrar")
+
+})
+
+$("#btn-hideShow").dblclick(function(){
+
+    $("p").slideDown();
+    $("#btn-hideShow").text("Esconder")
+
+})
+
+$("#btn-newP").click(function(){
+
+    $("#Primero").after("<p> Hola <strong> Programadores</strong></p>");
+
+})
+
+})
